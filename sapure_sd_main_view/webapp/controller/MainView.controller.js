ccdc3e6b7e7981bb96369cec92b85e27db346b2f
Hyun Su <trevor0910@naver.com>
2024-11-27 12:46:50 +0900

    11/27까지 수정본

--- sapure_sd_main_view/webapp/controller/MainView.controller.js
+++ sapure_sd_main_view/webapp/controller/MainView.controller.js
@@ -1,7 +1,6 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
-],
-function (Controller) {
+], function (Controller) {
     "use strict";
 
     return Controller.extend("zc503sd.gw0006.sapuresdmainview.controller.MainView", {
@@ -10,13 +9,18 @@ function (Controller) {
             var oImageModel = new sap.ui.model.json.JSONModel();
             oImageModel.loadData("model/images.json");
             this.getView().setModel(oImageModel, "imageModel");
+
+            // Hover 이벤트 연결
+            this._attachHoverEvents();
         },
-        onCollapseExpandPress() {
+
+        onCollapseExpandPress: function () {
             const oSideNavigation = this.byId("sideNavigation"),
-              bExpanded = oSideNavigation.getExpanded();
-    
+                bExpanded = oSideNavigation.getExpanded();
+
             oSideNavigation.setExpanded(!bExpanded);
-          },
+        },
+
         _startCarouselAutoSlide: function () {
             var oCarousel = this.byId("productImages");
             var iSlideIndex = 0; // 현재 슬라이드 인덱스
@@ -28,14 +32,22 @@ function (Controller) {
                 oCarousel.setActivePage(oCarousel.getPages()[iSlideIndex]); // 슬라이드 전환
             }, 3000); // 3000ms = 3초
         },
-        onHoverChange: function (oEvent) {
-            // StandardListItem의 customData에서 값 읽기
-            var oCustomData = oEvent.getSource().getCustomData();
-            var sImagePath = oCustomData.find(data => data.getKey() === "imagePath").getValue();
-        
-            // 이미지 컴포넌트를 찾아 src 속성 업데이트
-            var oDynamicImage = this.byId("dynamicImage");
-            oDynamicImage.setSrc(sImagePath);
-        }        
+
+        _attachHoverEvents: function () {
+            // List 컨트롤의 모든 StandardListItem 가져오기
+            const oList = this.byId("selectionList"); // List의 ID를 할당해야 합니다
+            const oItems = oList.getItems();
+
+            // 각 아이템에 Hover 이벤트 추가
+            oItems.forEach(item => {
+                item.addEventDelegate({
+                    onmouseover: () => {
+                        const aCustomData = item.getCustomData();
+                        const sImagePath = aCustomData.find(data => data.getKey() === "imagePath").getValue();
+                        this.byId("dynamicImage").setSrc(sImagePath);
+                    }
+                });
+            });
+        },
     });
 });
